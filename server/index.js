39f80abc323a7a94d72bb55f6cc8f452ef52a96a
Aaron Hiniker <hindog@gmail.com>
2023-12-22 13:43:26 -0800

    Serve static files under prefix

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const Koa = require('koa')
 const serve = require('koa-static')
+const mount = require('koa-mount')
+
 const Router = require('@koa/router')
 const portfinder = require('portfinder')
 const open = require('open')
@@ -13,6 +15,9 @@ function createServer ({
 }) {
   const app = new Koa()
 
+  var mountPath = routerPrefix
+  if (mountPath === '') mountPath = '/'
+
   const router = new Router({ prefix: routerPrefix })
 
   router.get('/config.json', (ctx) => {
@@ -20,7 +25,14 @@ function createServer ({
   })
 
   app
-    .use(serve(`${__dirname}/../dist`))
+    .use((ctx, next) => {
+      if (ctx.url === routerPrefix) {
+        return ctx.redirect(routerPrefix + '/index.html')
+      } else {
+        return next()
+      }
+    })
+    .use(mount(mountPath, serve(`${__dirname}/../dist`)))
     .use(router.routes())
     .use(router.allowedMethods())
 
